2bec2b63d8a8a5200556af900b7d5b131d3863d0
karolwolny <kwolny31@gmail.com>
2024-08-07 19:03:43 +0200

    Initial commit

---
+++ src/main/java/com/mrrevo/revoemoticons/Constants.java
@@ -0,0 +1,10 @@
+package com.mrrevo.revoemoticons;
+
+import lombok.AccessLevel;
+import lombok.NoArgsConstructor;
+
+@NoArgsConstructor(access = AccessLevel.PRIVATE)
+public class Constants {
+
+    public static final String PREFIX_PLUGIN = "[RevoEmoticons]";
+}
---
+++ src/main/java/com/mrrevo/revoemoticons/Plugin.java
@@ -0,0 +1,18 @@
+package com.mrrevo.revoemoticons;
+
+import lombok.extern.slf4j.Slf4j;
+import org.bukkit.plugin.java.JavaPlugin;
+
+@Slf4j
+public class Plugin extends JavaPlugin {
+
+    @Override
+    public void onDisable() {
+        super.onDisable();
+    }
+
+    @Override
+    public void onEnable() {
+        super.onEnable();
+    }
+}
